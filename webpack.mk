ac93470ce9207a04fe86e11d19fdb99eb328a959
Susan Dreher <sdreher@columbia.edu>
2016-07-22 10:26:34 -0400

    add dentalruralhealth-pack to the build

--- webpack.mk
+++ webpack.mk
@@ -6,3 +6,4 @@ webpack:  $(JS_SENTINAL)
 	$(WEBPACK_UTIL) --output-path $(OUTPUT_PATH)supportservices/ --config ./node_modules/supportservices-pack/webpack.config.js
 	$(WEBPACK_UTIL) --output-path $(OUTPUT_PATH)specialneedsvisit/ --config ./node_modules/specialneedsvisit-pack/webpack.config.js
 	$(WEBPACK_UTIL) --output-path $(OUTPUT_PATH)dentaloffice/ --config ./node_modules/dentalofficelocation-pack/webpack.config.js
+	$(WEBPACK_UTIL) --output-path $(OUTPUT_PATH)ruralhealth/ --config ./node_modules/dentalruralhealth-pack/webpack.config.js
